33bfd3c1334c88a37c5242381db6e0f30a60975d
Harsh Vitra <jainharsh44@gmail.com>
2021-04-01 16:13:01 +0530

    Initiated  project

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h2>Pay with Planet</h2>
+    </div>
+  )
+}
