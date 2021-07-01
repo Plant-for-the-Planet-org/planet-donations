578b93286dd4d1b184d388d1c931b7ed66a3236d
Harsh Vitra <jainharsh44@gmail.com>
2021-07-01 13:45:40 +0530

    Minimum amount should not shwo before configured

--- src/Donations/Components/DonationsForm.tsx
+++ src/Donations/Components/DonationsForm.tsx
@@ -300,12 +300,14 @@ function DonationsForm() {
                   </div>
                 )
               ) : (
+                minAmt > 0 ?
                 <p className={"text-danger mt-20 text-center"}>
                   {t("minDonate")}{" "}
                   <span>
                     {getFormatedCurrency(i18n.language, currency, minAmt)}
                   </span>
                 </p>
+                : <></>
               )
             ) : (
               <div className="mt-20 w-100">
