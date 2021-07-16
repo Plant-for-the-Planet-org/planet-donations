d886b0aa99a5a69b205b9c21f998d691e387497f
Harsh Vitra <jainharsh44@gmail.com>
2021-07-16 15:31:17 +0530

    Update themeProperties.ts

--- styles/themeProperties.ts
+++ styles/themeProperties.ts
@@ -27,7 +27,7 @@ const themeProperties = {
     highlightBackground: '#f2f2f2',
     light: '#fff',
     dark: '#000',
-    dangerColor:'#00ef00',
+    dangerColor:'#FF0000',
     backgroundColorDark: '#252627'
   },
 };
