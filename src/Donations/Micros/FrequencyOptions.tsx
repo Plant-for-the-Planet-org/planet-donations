7b2bf23245a58367dc654eaa7155308b0f8b6b49
Tejas Sonar <tejassonar227@gmail.com>
2021-08-30 16:53:07 +0530

    eslint errors

--- src/Donations/Micros/FrequencyOptions.tsx
+++ src/Donations/Micros/FrequencyOptions.tsx
@@ -4,14 +4,15 @@ import { QueryParamContext } from "../../Layout/QueryParamContext";
 interface Props {}
 
 function FrequencyOptions({}: Props): ReactElement {
-  const { paymentSetup, setfrequency,frequency } = React.useContext(QueryParamContext);
+  const { paymentSetup, setfrequency, frequency } =
+    React.useContext(QueryParamContext);
 
-  const customfrequencies = ["once","monthly","annually"];
+  const customfrequencies = ["once", "monthly", "annually"];
 
   return (
     <div className="d-flex justify-content-between flex-wrap frequency-selection-container mt-20">
       {paymentSetup.frequencies && paymentSetup.frequencies.length > 0 ? (
-        customfrequencies.map((frequencyOption:any, index:any) => {
+        customfrequencies.map((frequencyOption: any, index: any) => {
           return (
             <div
               className={`frequency-selection-option ${
@@ -19,9 +20,8 @@ function FrequencyOptions({}: Props): ReactElement {
                   ? "frequency-selection-option-selected"
                   : ""
               }`}
-              onClick={
-                  ()=>setfrequency(frequencyOption)
-              }
+              key={index}
+              onClick={() => setfrequency(frequencyOption)}
             >
               {frequencyOption}
             </div>
