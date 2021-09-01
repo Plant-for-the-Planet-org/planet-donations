4a1b08108b7811ed987872c6a9e7edee4c25cf3a
Harsh Vitra <jainharsh44@gmail.com>
2021-09-01 15:19:09 +0530

    Added tenant image for sitex

--- src/Utils/getTenantBackground.ts
+++ src/Utils/getTenantBackground.ts
@@ -1,10 +1,12 @@
 import defaultForest from '../../public/tenants/default/default-forest.jpg'
 import klumforest from '../../public/tenants/leniklum/leniklum.jpg'
+import sitex from '../../public/tenants/sitex/sitex.png'
 
 export function getTenantBackground(tenant:any){
     switch(tenant){
         case 'ten_I9TW3ncG': return defaultForest;
         case 'ten_KRHYP8TR': return klumforest;
+        case 'ten_cFbEAF7H': return sitex;
         default : return defaultForest;
     }
 }
