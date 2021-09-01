c1f420c01ee201f6a09557a5b68e9c7b23243c9a
Tejas Sonar <tejassonar227@gmail.com>
2021-09-01 16:08:58 +0530

    removed hyperlink if isPrivate is True

--- src/Donations/Micros/Authentication.tsx
+++ src/Donations/Micros/Authentication.tsx
@@ -118,27 +118,60 @@ function Authentication({}: Props): ReactElement {
 
       {(!isLoading && isAuthenticated && profile) || (queryToken && profile) ? (
         <div className="d-flex row justify-content-between w-100 mb-20">
-          <a
-            href={`https://www1.plant-for-the-planet.org/t/${profile.slug}`}
-            target={"_blank"}
-            rel="noreferrer"
-            className="user-profile"
-          >
-            {profile.image ? (
-              <img
-                className="profile-pic"
-                src={getImageUrl("profile", "avatar", profile.image)}
-                alt={profile ? profile.displayName : user?.name}
-              />
-            ) : user?.picture ? (
-              <img className="profile-pic" src={user.picture} alt={user.name} />
-            ) : (
-              <div className="profile-pic no-pic">
-                {profile ? profile.displayName.charAt(0) : user?.name.charAt(0)}
+          {!profile.isPrivate ? (
+            <a
+              href={`https://www1.plant-for-the-planet.org/t/${profile.slug}`}
+              target={"_blank"}
+              rel="noreferrer"
+              className="user-profile"
+            >
+              {profile.image ? (
+                <img
+                  className="profile-pic"
+                  src={getImageUrl("profile", "avatar", profile.image)}
+                  alt={profile ? profile.displayName : user?.name}
+                />
+              ) : user?.picture ? (
+                <img
+                  className="profile-pic"
+                  src={user.picture}
+                  alt={user.name}
+                />
+              ) : (
+                <div className="profile-pic no-pic">
+                  {profile
+                    ? profile.displayName.charAt(0)
+                    : user?.name.charAt(0)}
+                </div>
+              )}
+              <p>{profile ? profile.displayName : user?.name}</p>
+            </a>
+          ) : (
+            <div className="user-profile">
+              {profile.image ? (
+                <img
+                  className="profile-pic"
+                  src={getImageUrl("profile", "avatar", profile.image)}
+                  alt={profile ? profile.displayName : user?.name}
+                />
+              ) : user?.picture ? (
+                <img
+                  className="profile-pic"
+                  src={user.picture}
+                  alt={user.name}
+                />
+              ) : (
+                <div className="profile-pic no-pic">
+                  {profile
+                    ? profile.displayName.charAt(0)
+                    : user?.name.charAt(0)}
+                </div>
+              )}
+              <div className="profile-name">
+                {profile ? profile.displayName : user?.name}
               </div>
-            )}
-            <p>{profile ? profile.displayName : user?.name}</p>
-          </a>
+            </div>
+          )}
           {user ? (
             <button
               className="login-continue"
