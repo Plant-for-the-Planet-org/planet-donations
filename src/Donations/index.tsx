b3c78ffffa766f80d87e64e2b38936ff55793fdd
Tejas Sonar <tejassonar227@gmail.com>
2021-11-19 14:56:35 +0530

    added frequency on left panel

--- src/Donations/index.tsx
+++ src/Donations/index.tsx
@@ -83,6 +83,7 @@ function DonationInfo() {
     giftDetails,
     isGift,
     tenant,
+    frequency,
   } = React.useContext(QueryParamContext);
 
   const TPOImage = () => {
@@ -143,7 +144,9 @@ function DonationInfo() {
             projectDetails.purpose === "trees" && (
               <div className="contact-details-info">
                 <div className={"w-100 mt-10 text-white"}>
-                  {t("donating")}
+                  {t("donating", {
+                    frequency: t(`${frequency}`),
+                  })}
                   <span className="text-bold" style={{ marginRight: "4px" }}>
                     {getFormatedCurrency(
                       i18n.language,
@@ -166,7 +169,9 @@ function DonationInfo() {
             projectDetails.purpose === "bouquet" && (
               <div className="contact-details-info">
                 <div className={"w-100 mt-10 text-white"}>
-                  {t("donating")}
+                  {t("donating", {
+                    frequency: t(`${frequency}`),
+                  })}
                   <span className="text-bold" style={{ marginRight: "4px" }}>
                     {getFormatedCurrency(
                       i18n.language,
