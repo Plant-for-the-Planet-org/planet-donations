fb1f7297fa97395eb3f7e1bf36e4272936c8ae47
Norbert Schuler <norbert.schuler@epublica.de>
2021-12-08 23:20:56 +0100

    allow to enter decimal numbers and do not use paymentSetup.unitCost

--- src/Donations/Micros/DonationTypes/BouquetDonations.tsx
+++ src/Donations/Micros/DonationTypes/BouquetDonations.tsx
@@ -36,9 +36,9 @@ function FundingDonations({ setopenCurrencyModal }: Props): ReactElement {
     if (e.target) {
       if (e.target.value === "" || e.target.value < 1) {
         // if input is '', default 1
-        setquantity(1 / paymentSetup.unitCost);
+        setquantity(paymentSetup.unitBased ? 1 / paymentSetup.unitCost : 1);
       } else if (e.target.value.toString().length <= 12) {
-        setquantity(e.target.value / paymentSetup.unitCost);
+        setquantity(paymentSetup.unitBased ? e.target.value / paymentSetup.unitCost : e.target.value);
       }
     }
   };
@@ -53,7 +53,10 @@ function FundingDonations({ setopenCurrencyModal }: Props): ReactElement {
         newallOptionsArray.push(option.quantity);
       }
       if (!newallOptionsArray.includes(quantity)) {
-        setCustomInputValue(quantity * paymentSetup.unitCost);
+        setCustomInputValue(paymentSetup.unitBased ? quantity * paymentSetup.unitCost : quantity);
+        setisCustomDonation(true);
+      } else {
+        setisCustomDonation(false);
       }
     }
   }, [paymentSetup]);
@@ -141,7 +144,8 @@ function FundingDonations({ setopenCurrencyModal }: Props): ReactElement {
                   className={"funding-custom-tree-input"}
                   onInput={(e) => {
                     // replaces any character other than number to blank
-                    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+                    e.target.value = e.target.value.replace(/[,]/g, '.');
+                    e.target.value = e.target.value.replace(/[^0-9\.]/g, "");
                     //  if length of input more than 12, display only 12 digits
                     if (e.target.value.toString().length >= 12) {
                       e.target.value = e.target.value.toString().slice(0, 12);
