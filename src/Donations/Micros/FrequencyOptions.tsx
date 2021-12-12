a30919afe0870835da18d9bf73f900f0d3c5e6e2
Tejas Sonar <tejassonar227@gmail.com>
2021-12-12 16:55:34 +0530

    fixed eslint error

--- src/Donations/Micros/FrequencyOptions.tsx
+++ src/Donations/Micros/FrequencyOptions.tsx
@@ -2,14 +2,14 @@ import { useTranslation } from "next-i18next";
 import React, { ReactElement } from "react";
 import { QueryParamContext } from "../../Layout/QueryParamContext";
 
-interface Props { }
+interface Props {}
 
-function FrequencyOptions({ }: Props): ReactElement {
+function FrequencyOptions({}: Props): ReactElement {
   const { paymentSetup, setfrequency, frequency } =
     React.useContext(QueryParamContext);
   const { t } = useTranslation(["common"]);
-  
-  let customfrequencies = paymentSetup.frequencies;
+
+  const customfrequencies = paymentSetup.frequencies;
   // let customfrequencies = ["once", "monthly", "yearly"];
   // if (paymentSetup.purpose === "funds") {
   //   customfrequencies = ["monthly", "yearly"];
@@ -22,10 +22,11 @@ function FrequencyOptions({ }: Props): ReactElement {
         customfrequencies?.map((frequencyOption: any, index: any) => {
           return (
             <div
-              className={`frequency-selection-option ${frequencyOption === frequency
+              className={`frequency-selection-option ${
+                frequencyOption === frequency
                   ? "frequency-selection-option-selected"
                   : ""
-                }`}
+              }`}
               key={index}
               onClick={() => setfrequency(frequencyOption)}
               data-test-id="frequency"
