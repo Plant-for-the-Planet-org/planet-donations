b5fa3e46429af4d55668e6f5396b92a116d97445
Sarvesh Warge <sarveshwarge@gmail.com>
2021-12-17 05:13:45 +0530

    check if node env is production

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
   // ...
