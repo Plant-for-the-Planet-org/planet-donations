73fec835938d1639648728496a93bb641ad71187
Tejas Sonar <tejassonar227@gmail.com>
2022-02-23 12:10:14 +0530

    show default image for project selection page

--- src/Donations/index.tsx
+++ src/Donations/index.tsx
@@ -142,9 +142,10 @@ function DonationInfo() {
   return (
     <div
       style={{
-        backgroundImage: projectDetails?.projectImage
-          ? `url(${getTenantBackground(tenant, projectDetails)})`
-          : "none",
+        backgroundImage:
+          projectDetails?.projectImage || donationStep === 0
+            ? `url(${getTenantBackground(tenant, projectDetails)})`
+            : "none",
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
