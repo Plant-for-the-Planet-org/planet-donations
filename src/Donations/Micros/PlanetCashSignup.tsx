348ab852783f71bdfb33992d5ec7411e6b0d8374
Shreyas Chorge <shreyaschorge81@gmail.com>
2022-06-21 20:37:07 +0530

    Add Select country dropdown in planetCash create account

--- src/Donations/Micros/PlanetCashSignup.tsx
+++ src/Donations/Micros/PlanetCashSignup.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "next-i18next";
+import CountrySelect from "src/Common/InputTypes/AutoCompleteCountry";
 
 const PlanetCashSignup = () => {
   const { t } = useTranslation(["common"]);
+
+  const [country, setCountry] = useState("DE");
+
   return (
     <div
       style={{
@@ -15,6 +19,15 @@ const PlanetCashSignup = () => {
       <div>
         <p className="title-text mb-20">{t("planetCashSignup")}</p>
         <p className="mb-20">{t("noPlanetCashAccount")}</p>
+        <div className={"form-field mb-20"}>
+          <CountrySelect
+            allowedCountries={["DE", "ES", "US"]}
+            label={t("country")}
+            name="country"
+            onChange={(e) => setCountry(e.target?.value)}
+            defaultValue={country}
+          />
+        </div>
         <p className="mb-20">{t("planetCashTC")}</p>
         <p>{t("planetCashIUnderstand")}</p>
       </div>
