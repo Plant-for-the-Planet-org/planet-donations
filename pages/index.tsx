5b97136e8ff2d96dd57377821fc4a231d90f7736
Shreyas Chorge <shreyaschorge81@gmail.com>
2022-11-03 10:21:32 +0530

    Fix Redirect to Contact Form

--- pages/index.tsx
+++ pages/index.tsx
@@ -299,8 +299,6 @@ export async function getServerSideProps(context: any) {
       const queryMethodForStep4 = ["Sofort", "Giropay"];
       const queryRedirectStatus = ["succeeded", "failed"];
 
-      let paymentOptionsCountry;
-
       if (donation.status === 200) {
         const donorData = donation.data.donor;
         donationID = context.query.context;
@@ -333,7 +331,6 @@ export async function getServerSideProps(context: any) {
           };
           const paymentSetupData: any = await apiRequest(requestParams);
           if (paymentSetupData.data) {
-            paymentOptionsCountry = paymentSetupData.data.country;
             currency = paymentSetupData.data.currency;
             paymentSetup = paymentSetupData.data;
             projectDetails = {
@@ -387,12 +384,7 @@ export async function getServerSideProps(context: any) {
           // Check if all contact details are present - if not send user to step 2 else step 3
           // Check if all payment cards are present - if yes then show it on step 3
           isDirectDonation = true;
-
-          // If paymentOptionsCountry is brazil only Card and boleto is supported
-          // For boleto payment cnpOrCnjpNumber
-          if (paymentOptionsCountry && paymentOptionsCountry == "BR") {
-            donationStep = 2;
-          } else donationStep = 3;
+          donationStep = 3;
         }
       } else {
         // SET Error that no donation is found
