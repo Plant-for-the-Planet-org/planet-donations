6c2e2bebb61f442fd9c17abca433dadc38df98e2
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-02-27 11:56:34 +0530

    feat: update next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,7 @@
-const path = require('path');
 module.exports = {
   i18n: {
     // debug: true,
-    localeDetection: false,
     defaultLocale: "en",
     locales: ["en", "cs", "de", "it", "es", "fr", "pt-BR"],
-    localePath: path.resolve('./public/locales')
   },
 };
