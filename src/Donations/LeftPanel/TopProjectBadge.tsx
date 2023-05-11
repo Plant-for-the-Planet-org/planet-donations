3dbffdb779e8ffa52ff8ccc7b54142ebe9e27d2a
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-05-11 09:05:01 +0530

    fix: make top project badge translatable

--- src/Donations/LeftPanel/TopProjectBadge.tsx
+++ src/Donations/LeftPanel/TopProjectBadge.tsx
@@ -1,8 +1,10 @@
 import { ReactElement } from "react";
+import { useTranslation } from "next-i18next";
 import styles from "./LeftPanel.module.scss";
 
 const TopProjectBadge = (): ReactElement => {
-  return <div className={styles["top-project-badge"]}>Top Project</div>;
+  const { t } = useTranslation("common");
+  return <div className={styles["top-project-badge"]}>{t("topProject")}</div>;
 };
 
 export default TopProjectBadge;
