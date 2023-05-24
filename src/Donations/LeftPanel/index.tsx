a5a7f082311185ad71f85736b3df16c685748312
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-05-24 20:31:30 +0530

    feat: add cancel button

--- src/Donations/LeftPanel/index.tsx
+++ src/Donations/LeftPanel/index.tsx
@@ -1,10 +1,10 @@
 import { ReactElement, useContext, useState, useEffect } from "react";
 import LeftPanelContainer from "./LeftPanelContainer";
-import BackButton from "../../Common/BackButton";
 import { QueryParamContext } from "../../Layout/QueryParamContext";
 import LeftPanelHeader from "./LeftPanelHeader";
 import TopProjectBadge from "./TopProjectBadge";
 import LeftPanelInfo from "./LeftPanelInfo";
+import CancelButton from "../../Common/CancelButton";
 
 function LeftPanel(): ReactElement {
   const {
@@ -53,7 +53,7 @@ function LeftPanel(): ReactElement {
       tenant={tenant}
     >
       <LeftPanelHeader>
-        {canShowBackButton && <BackButton backUrl={callbackUrl || "/"} />}
+        {canShowBackButton && <CancelButton returnUrl={callbackUrl || "/"} />}
         {canShowTopProjectBadge && <TopProjectBadge />}
       </LeftPanelHeader>
       {/* TODO - evaluate whether to send this info to LeftPanelInfo, or use context instead */}
