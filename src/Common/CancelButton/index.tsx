7c210deb203890c242055574404aa399e19f7878
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-05-25 20:57:59 +0530

    feat: add title on button

--- src/Common/CancelButton/index.tsx
+++ src/Common/CancelButton/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
 import styles from "./CancelButton.module.scss";
 import CloseIcon from "public/assets/icons/CloseIcon";
+import { QueryParamContext } from "src/Layout/QueryParamContext";
 
 interface Props {
   returnUrl: string;
@@ -9,13 +10,18 @@ interface Props {
 
 const CancelButton = ({ returnUrl }: Props): ReactElement => {
   const router = useRouter();
+  const { callbackUrl } = useContext(QueryParamContext);
 
   const cancelDonation = () => {
     router.push(returnUrl);
   };
 
   return (
-    <button className={styles["cancel-button"]} onClick={cancelDonation}>
+    <button
+      className={styles["cancel-button"]}
+      onClick={cancelDonation}
+      title={callbackUrl ? "Exit" : "Home"}
+    >
       <CloseIcon color={"#000"} />
     </button>
   );
