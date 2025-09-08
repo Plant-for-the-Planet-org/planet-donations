e675fa0de7ec80c1b357d118e9bb55df15456285
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-09-08 17:31:12 +0800

    fix: Update TODO comment for supported projects in donation config

--- src/Utils/supportedDonationConfig.ts
+++ src/Utils/supportedDonationConfig.ts
@@ -12,6 +12,7 @@ export const supportedDonationConfig: SupportedDonationConfig = {
   ten_3hEjJCBs: {
     supportPercentage: 0.25,
     supportedProjects: {
+      // TODO: To be updated once list of supported projects is confirmed
       DE: "proj_bFH0BU0Qw02RuetpQlLOMVYX",
       US: "proj_bFH0BU0Qw02RuetpQlLOMVYX",
       default: "proj_bFH0BU0Qw02RuetpQlLOMVYX",
