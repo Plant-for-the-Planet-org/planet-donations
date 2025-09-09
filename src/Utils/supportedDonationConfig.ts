486a3f37bb3d98efa822f137111b786f46dbefe9
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-09-09 17:38:06 +0800

    feat: update tenant in supportedDonationConfig

--- src/Utils/supportedDonationConfig.ts
+++ src/Utils/supportedDonationConfig.ts
@@ -3,19 +3,15 @@ export interface SupportedDonationConfig {
     supportPercentage: number;
     supportedProjects: {
       [countryCode: string]: string;
-      default: string;
     };
   };
 }
 
 export const supportedDonationConfig: SupportedDonationConfig = {
-  ten_3hEjJCBs: {
+  ten_670y1ZHn: {
     supportPercentage: 0.25,
     supportedProjects: {
-      // TODO: To be updated once list of supported projects is confirmed
       DE: "proj_bFH0BU0Qw02RuetpQlLOMVYX",
-      US: "proj_bFH0BU0Qw02RuetpQlLOMVYX",
-      default: "proj_bFH0BU0Qw02RuetpQlLOMVYX",
     },
   },
 };
